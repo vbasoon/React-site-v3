1c0d5683a7ddbb9336c2194ece308337803eeb1d
Басун Володимир <vbasoon@gmail.com>
2021-02-12 16:51:33 +0200

    navElements

--- src/components/Navbar/NavElements.js
+++ src/components/Navbar/NavElements.js
@@ -41,4 +41,41 @@ export const NavLogo = styled(LinkR)`
    text-decoration: none;
    `
 
-//export const MobileIcon
+export const MobileIcon = styled.div`
+display: none;
+
+@media screen and (max-width: 768px) {
+   display: block;
+   position: absolute;
+   top: 0;
+   right: 0;
+   transition: trnaslate(-100%, 60%);
+   font-size: 1.8rem;
+   cursor: pointer;
+   }
+`
+
+export const NavMenu = styled.ul`
+   displey: flex;
+   align-items: center;
+   list-style: none;
+   text-align: center;
+   margin-right: -22px;
+
+   @media screen and (max-width: 768px) {
+      display: none;
+   }
+`
+export const NavLinks = styled(LinkS)`
+   color: #ffffff;
+   display: flex;
+   align-items: center;
+   text-decoration: none;
+   padding: 0 1rem;
+   height: 100%;
+   cursor: pointer;
+
+   &.active {
+      border-bottom: 3px solid #01bf71;
+   }
+`;
