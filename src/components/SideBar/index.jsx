f2171c189b5dc246e8c23f5660bf2569a2897134
Басун Володимир <vbasoon@gmail.com>
2021-02-15 07:56:49 +0200

    Sidebar

---
+++ src/components/SideBar/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Sidebar = () => {
+   return (
+      <SidebarContainefr>
+         <Icon>
+            <CloseIcon />
+         </Icon>
+      </SidebarContainefr>
+   )
+}
+
+export default Sidebar
