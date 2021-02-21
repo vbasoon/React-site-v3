5971350d54e3203a4adff4074a7ef1d6b7ec3984
Басун Володимир <vbasoon@gmail.com>
2021-02-21 09:31:31 +0200

    Scroll Effect

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import { FaBars } from 'react-icons/fa'
-import {IconContext} from 'react-icons/lib' 
+import {IconContext} from 'react-icons/lib'
+import {animateScroll as scroll} from 'react-scroll' 
 import {
    Nav,
    NavbarContainer,
@@ -28,14 +29,18 @@ const Navbar = ({ toggle }) => {
 
    useEffect(() => {
       window.addEventListener('scroll', changeNav)
-   }, []);  
+   }, []);
+   
+   const toggleHome = () => {
+      scroll.scrollToTop();
+   }
 
    return (
       <>
-      <IconContext.Provider value={{color: 'red'}}>
+      <IconContext.Provider value={{color: '#fff'}}>
          <Nav scrollNav={scrollNav}>
             <NavbarContainer>
-               <NavLogo to='/'>
+               <NavLogo to='/' onClick={toggleHome}>
                   <h1>LCoin</h1>
                </NavLogo>
                <MobileIcon onClick={toggle}>
